ea9f76f84e00c24f19d17e3c17ab954609ed5ef2
Pedro Dias <jpedrodias@gmail.com>
2025-01-18 13:05:25 +0000

    ex07.py - consumo por litro

--- UFCD10793_FT01/ex07.py
+++ UFCD10793_FT01/ex07.py
@@ -3,8 +3,6 @@ Faz um programa que receba a distância em km e a quantidade em litros de
 combustível consumido por um carro num percurso. Calcula o consumo km/l e escreve
 uma mensagem de acordo com o resultado obtido. (deverá ser criado o ficheiro
 ex07.py).
-
-Exemplo: python ex07.py 100 10
 '''
 
 distancia = float(input('Introduza a distância percorrida em km: '))
