45c9003c5d1e9604473b0aa9981e6d4616bd9b90
Pedro Dias <jpedrodias@gmail.com>
2025-01-18 12:25:36 +0000

    ex05

--- UFCD10793_FT01/ex05.py
+++ UFCD10793_FT01/ex05.py
@@ -6,25 +6,7 @@ Sejam a e b os catetos de um triângulo retângulo, faz um programa que devolva 
 
 from math import sqrt
 
-
-def calcula_hipotenusa(a, b):
-    __doc__ = 'Calcula a hipotenusa de um triângulo retângulo'
-    return sqrt(a**2 + b**2)
-
-
-if __name__ == '__main__':
-    a_str = input('Introduza o valor do cateto a: ')
-    b_str = input('Introduza o valor do cateto b: ')
-    try:
-        a = float(a_str)
-        b = float(b_str)
-    except ValueError:
-        a = None
-        b = None
-    
-    if a is not None and b is not None:
-        hipotenusa = calcula_hipotenusa(a, b)
-        print(f'O valor da hipotenusa é {hipotenusa:.2f}')
-    else:
-        print(f'Valor inválido: {a_str} ou {b_str}')
-#end if
+cateto1 = int(input('Introduza o valor do cateto 1: '))
+cateto2 = int(input('Introduza o valor do cateto 2: '))
+hipotenusa = sqrt(cateto1**2 + cateto2**2)
+print(f'O valor da hipotenusa é {hipotenusa:.2f}')
---
+++ UFCD10793_FT01/ex05b.py
@@ -0,0 +1,30 @@
+'''
+Sejam a e b os catetos de um triângulo retângulo, faz um programa que devolva o valor da hipotenusa (deverá ser criado o ficheiro ex05.py).
+
+'''
+
+
+from math import sqrt
+
+
+def calcula_hipotenusa(a, b):
+    __doc__ = 'Calcula a hipotenusa de um triângulo retângulo'
+    return sqrt(a**2 + b**2)
+
+
+if __name__ == '__main__':
+    a_str = input('Introduza o valor do cateto a: ')
+    b_str = input('Introduza o valor do cateto b: ')
+    try:
+        a = float(a_str)
+        b = float(b_str)
+    except ValueError:
+        a = None
+        b = None
+    
+    if a is not None and b is not None:
+        hipotenusa = calcula_hipotenusa(a, b)
+        print(f'O valor da hipotenusa é {hipotenusa:.2f}')
+    else:
+        print(f'Valor inválido: {a_str} ou {b_str}')
+#end if
