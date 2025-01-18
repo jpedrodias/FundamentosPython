e9a7f2fad1eb8e9f513940f9a9912db1d074e87e
Pedro Dias <jpedrodias@gmail.com>
2025-01-18 12:27:53 +0000

    ex07

--- UFCD10793_FT01/ex07.py
+++ UFCD10793_FT01/ex07.py
@@ -7,24 +7,7 @@ ex07.py).
 Exemplo: python ex07.py 100 10
 '''
 
-import sys
-
-def calcula_consumo(distancia, litros, precisao=2):
-    __doc__ = 'Calcula o consumo em km/l'
-    return round(distancia / litros, precisao)
-
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        try:
-            distancia, litros = [float(f) for f in sys.argv[1:]]
-        except ValueError:
-            distancia, litros = None, None
-
-        if distancia is not None and litros is not None:
-            consumo = calcula_consumo(distancia, litros)
-            print(f'O consumo foi de {consumo} km/l')
-        else:
-            print('Valores inválidos\nExemplo: python ex07.py 100 10')
-    else:
-        print('Número de argumentos inválido\nExemplo: python ex07.py 100 10')
-#end if __main__
+distancia = float(input('Introduza a distância percorrida em km: '))
+litros = float(input('Introduza a quantidade de litros consumidos: '))
+consumo = distancia / litros
+print(f'O consumo foi de {consumo:.2f} km/l')
