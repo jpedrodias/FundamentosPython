d38e0c06c1883a21557279608336e0427b5b1aec
Pedro Dias <jpedrodias@gmail.com>
2025-01-18 12:31:06 +0000

    ex05.py - hipotenusa

--- UFCD10793_FT01/ex05.py
+++ UFCD10793_FT01/ex05.py
@@ -1,9 +1,6 @@
 '''
 Sejam a e b os catetos de um triângulo retângulo, faz um programa que devolva o valor da hipotenusa (deverá ser criado o ficheiro ex05.py).
-
 '''
-
-
 from math import sqrt
 
 cateto1 = int(input('Introduza o valor do cateto 1: '))
