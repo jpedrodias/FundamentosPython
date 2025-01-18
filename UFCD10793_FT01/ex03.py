1cee85713cf0bad1633121194f59182a13ac6729
Pedro Dias <jpedrodias@gmail.com>
2025-01-18 13:03:59 +0000

    ex03.py o dobro

--- UFCD10793_FT01/ex03.py
+++ UFCD10793_FT01/ex03.py
@@ -1,3 +1,7 @@
+'''
+Cria agora um nove ficheiro python (extensão .py) com o nome ex03.
+Dentro deste ficheiro escreve um programa que receba, como parâmetro um inteiro, e devolva o se dobro
+'''
 num = int(input('Intere um númeor inteiro'))
 dobro = num * 2
 print('O dobro desse valor é', dobro)
