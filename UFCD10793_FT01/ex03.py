cc546686c0226d993b3e5938ea9de84b338a7933
Pedro Dias <jpedrodias@gmail.com>
2025-01-25 11:53:14 +0000

    Exercício 3 - calcula o dobro de um número inteiro

--- UFCD10793_FT01/ex03.py
+++ UFCD10793_FT01/ex03.py
@@ -1,4 +1,6 @@
 '''
+Exercício 3 - calcula o dobro de um número inteiro
+
 Cria agora um nove ficheiro python (extensão .py) com o nome ex03.
 Dentro deste ficheiro escreve um programa que receba, como parâmetro um inteiro, e devolva o se dobro
 '''
