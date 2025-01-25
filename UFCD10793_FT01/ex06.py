dac7fff455ff7f3da55eacadd08f0aa6e38a93b4
Pedro Dias <jpedrodias@gmail.com>
2025-01-25 11:58:08 +0000

    Exercício 6 - calcula o total de segundos (versão mais comleta)

--- UFCD10793_FT01/ex06.py
+++ UFCD10793_FT01/ex06.py
@@ -1,4 +1,6 @@
 '''
+Exercício 6 - calcula o total de segundos
+
 Faz um programa que receba três parâmetros inteiros (horas, minutos e segundos) e
 converta o resultado para segundos, devolvendo o output para o ecrã (deverá ser
 criado o ficheiro ex06.py).
