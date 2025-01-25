777285c27c1b6a3641ec5d05b6887690a2996ba0
Pedro Dias <jpedrodias@gmail.com>
2025-01-25 11:58:42 +0000

    Exercício 7 - calcula o consumo por litro

--- UFCD10793_FT01/ex07.py
+++ UFCD10793_FT01/ex07.py
@@ -1,4 +1,6 @@
 '''
+Exercício 7 - calcula o consumo por litro
+
 Faz um programa que receba a distância em km e a quantidade em litros de
 combustível consumido por um carro num percurso. Calcula o consumo km/l e escreve
 uma mensagem de acordo com o resultado obtido. (deverá ser criado o ficheiro
