f1488275d963305237f38974cbdbdf4f887c4a72
Pedro Dias <jpedrodias@gmail.com>
2025-01-25 11:56:27 +0000

    Exercício 5 - calcula a hipotenusa

--- UFCD10793_FT01/ex05.py
+++ UFCD10793_FT01/ex05.py
@@ -1,4 +1,6 @@
 '''
+Exercício 5 - calcula a hipotenusa
+
 Sejam a e b os catetos de um triângulo retângulo, faz um programa que devolva o valor da hipotenusa (deverá ser criado o ficheiro ex05.py).
 '''
 from math import sqrt
