0b638f5137a837f0c6c2a770e00ccd06bcdb4f07
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 11:57:22 +0000

    Exercício 3 - Execussão condicional e operador módulo da divisão inteira

---
+++ UFCD10793_FT03/ex03.py
@@ -0,0 +1,16 @@
+'''
+Exercício 3 - Execussão condicional e operador módulo da divisão inteira
+Escreve um programa que solicite um número inteiro ao utilizador e verifique se o mesmo é par ou ímpar. A mensagem no ecrã deverá ter o seguinte formato; 
+"O número [número] é [par/ímpar]"
+'''
+
+
+# Input
+num = input("Insira um número inteiro: ")
+num = int(num)
+
+# Process
+if num % 2 == 0:
+    print(f"O número {num} é par.")
+else:
+    print(f"O número {num} é ímpar.")
