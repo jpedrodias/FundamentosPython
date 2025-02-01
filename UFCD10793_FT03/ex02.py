4b559f89179c1c4c8e02db63b25ca2a4a5abd5bd
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 12:10:25 +0000

    Exercício 2 - Execussão condicional e operador de igualdade

--- UFCD10793_FT03/ex02.py
+++ UFCD10793_FT03/ex02.py
@@ -1,11 +1,11 @@
 '''
-Exercício 2 - Execussão condicional e operador de comparação
+Exercício 2 - Execussão condicional e operador de igualdade
 Implementa um programa que leia dois números e indique se estes são iguais ou diferentes.
 '''
 
 # Input
-num1 = input("Insira um número inteiro: ")
-num2 = input("Insira outro número inteiro: ")
+num1 = input('Insira um número inteiro: ')
+num2 = input('Insira outro número inteiro: ')
 
 # Converte os números para inteiros
 num1 = int(num1)
@@ -13,6 +13,6 @@ num2 = int(num2)
 
 # Process
 if num1 == num2:
-    print(f"Os números {num1} e {num2} são iguais.")
+    print(f'Os números {num1} e {num2} são iguais.')
 else:
-    print(f"Os números {num1} e {num2} são diferentes.")
+    print(f'Os números {num1} e {num2} são diferentes.')
