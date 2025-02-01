1f28c00d01abe5d57050d96274458a88183caec8
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 13:03:31 +0000

    Exercício 7 - Ano bissexto

--- UFCD10793_FT03/ex07.py
+++ UFCD10793_FT03/ex07.py
@@ -1,6 +1,8 @@
 '''
 Exercício 7 - Ano bissexto
-Elabora um programa para verificar se um ano é bissexto ou não. A condição para ser um ano bissexto é: o ano deve ser divisível por 400;
+Elabora um programa para verificar se um ano é bissexto ou não. 
+A condição para ser um ano bissexto é: 
+o ano deve ser divisível por 400; 
 ou se for divisível por 4 e não for divisível por 100.
 '''
 
