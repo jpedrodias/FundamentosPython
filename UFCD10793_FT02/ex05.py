6d623deea7919eea4f6229618887e6a1f4016800
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 10:02:53 +0000

    ex05

---
+++ UFCD10793_FT02/ex05.py
@@ -0,0 +1,8 @@
+'''
+Num ficheiro diferente dos utilizados nas alíneas anteriores, escreve um programa que faça a conversão para kms, de um dado valor em metros.
+'''
+
+distancia_kms = input('Insere o valor em kms (em inteiro ou float): ')
+distancia_metros = float(distancia_kms) * 1000
+
+print(f'{distancia_kms} kms são {distancia_metros} metros')
