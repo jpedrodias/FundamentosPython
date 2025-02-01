7b6f314077f27f6506e485dcf8c6e297fa37862e
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 10:55:21 +0000

    Exercício 6 - função conversão temperaturas

--- UFCD10793_FT02/ex06.py
+++ UFCD10793_FT02/ex06.py
@@ -3,12 +3,17 @@ Exercício 6 - função conversão temperaturas
 Escreve um programa que solicite a temperatura em Fahrenheit (F), ao utilizador, e a converta para grau Celsius (C), devolvendo o resultado da conversão.
 '''
 
+# Solicitar a temperatura em Fahrenheit
 fahrenheit = input('Insere a temperatura em Fahrenheit: ')
 
+# Converter a temperatura para float
 fahrenheit = float(fahrenheit)
 
+# Calcular a temperatura em graus Celsius
 celsius = (fahrenheit - 32) * 5.0/9.0
 
+# Arredondar a temperatura para 2 casas decimais
 celsius = round(celsius, 2)
 
+# Apresentar a temperatura em graus Celsius
 print(f'A temperatura em graus Celsius é {celsius}')
