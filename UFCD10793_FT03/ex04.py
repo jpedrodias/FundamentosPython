28d7169c5224ac1ca98bdf580001bb675b58cd4f
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 12:11:46 +0000

    Exercício 4 - Execussão condicional e operadores de comparação maior ou menor que

--- UFCD10793_FT03/ex04.py
+++ UFCD10793_FT03/ex04.py
@@ -1,11 +1,11 @@
 '''
-Exercício 4 - Execussão condicional e operadores de comparação
+Exercício 4 - Execussão condicional e operadores de comparação maior ou menor que
 Escreve um programa que receba dois números reais e indique qual o maior dos dois  números. Considera a possibilidade de o utilizador indicar dois números iguais.
 '''
 
 # Input
-num1 = input("Insira um número real: ")
-num2 = input("Insira outro número real: ")
+num1 = input('Insira um número real: ')
+num2 = input('Insira outro número real: ')
 
 # Converte os números para reais
 num1 = float(num1)
@@ -13,8 +13,8 @@ num2 = float(num2)
 
 # Process
 if num1 > num2:
-    print(f"O número {num1} é maior que o número {num2}.")
+    print(f'O número {num1} é maior que o número {num2}.')
 elif num1 < num2:
-    print(f"O número {num2} é maior que o número {num1}.")
+    print(f'O número {num2} é maior que o número {num1}.')
 else:
-    print(f"Os números {num1} e {num2} são iguais.")
+    print(f'Os números {num1} e {num2} são iguais.')
