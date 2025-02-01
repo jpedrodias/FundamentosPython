d13bbcbaec17877b653820009f1e4d870d6e5dfe
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 12:12:26 +0000

    Exercício 5 - Execussão condicional e operadores de comparação de maior ou menor que

--- UFCD10793_FT03/ex05.py
+++ UFCD10793_FT03/ex05.py
@@ -1,16 +1,16 @@
 '''
-Exercício 5 - Execussão condicional e operadores de comparação
+Exercício 5 - Execussão condicional e operadores de comparação de maior ou menor que
 Escreva um programa que verifique se um determinado número introduzido pelo utilizador é nulo, positivo ou negativo. 
 '''
 
 # Input
-num = input("Insira um número inteiro: ")
+num = input('Insira um número inteiro: ')
 num = int(num)
 
 # Process
 if num > 0:
-    print(f"O número {num} é positivo.")
+    print(f'O número {num} é positivo.')
 elif num < 0:
-    print(f"O número {num} é negativo.")
+    print(f'O número {num} é negativo.')
 else:
-    print(f"O número {num} é nulo.")
+    print(f'O número {num} é nulo.')
