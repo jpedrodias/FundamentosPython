537f08f40531b3fda833e93e81a5a98cad311458
Pedro Dias <jpedrodias@gmail.com>
2025-02-01 11:34:48 +0000

    Exercício 7 - calcula o consumo por litro + msg

--- UFCD10793_FT01/ex07.py
+++ UFCD10793_FT01/ex07.py
@@ -9,26 +9,25 @@ ex07.py).
 
 distancia = float(input('Introduza a distância percorrida em km: '))
 litros = float(input('Introduza a quantidade de litros consumidos: '))
-consumo = distancia / litros
+consumo = litros / distancia  # litros por km
 
-msg = f'O consumo é de {consumo:.2f} km/l'
-if consumo > 10:
+msg = f'O consumo é de {consumo:.3f} L/km'
+if consumo > 0.10:
     msg += '''
 Consumo muito elevado.
 (SUVs grandes, veículos desportivos ou condução agressiva em cidade).'''
 
-elif consumo > 8:
+elif consumo > 0.08:
     msg += '''
 Consumo elevado.
 (SUVs médios ou grandes, veículos com motores mais potentes).'''
 
-elif consumo >= 6:
+elif consumo >= 0.06:
     msg += '''
 Consumo alto.
-(A maioria dos veículos ligeiros convencionais a gasolina ou Diesel).
-'''
+(A maioria dos veículos ligeiros convencionais a gasolina ou Diesel).'''
 
-elif consumo >= 4:
+elif consumo >= 0.04:
     msg += '''
 Consumo baixo.
 (Veículos compactos, híbridos convencionais ou a Diesel eficiente em rodovia).'''
