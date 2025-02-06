4535236d26748a6d11b32c46b3c770220573a51b
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:25:19 +0000

    Exercício 10 - while loop

---
+++ UFCD10793_FT04/ex10.py
@@ -0,0 +1,10 @@
+'''
+Exercício 10 - while loop
+Escreve um programa que coloque no ecrã os 10 primeiros números de forma decrescente.
+'''
+
+quantidade = 0
+i = 10
+while i > quantidade:
+    print(f'{i}')    
+    i -= 1
