4d86266e9a6b220489519458ca36fd825a3498ec
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:04:02 +0000

    Exercício 2 - match

---
+++ UFCD10793_FT04/ex02.py
@@ -0,0 +1,20 @@
+'''
+Exercício 2 - match
+Implemente um programa que, dada uma letra (S, C ou V), indique o estado civil de uma pessoa.
+'''
+
+# Input
+estado_civil = input('Insira o estado civil (S, C ou V): ')
+estado_civil = estado_civil.upper()
+
+match estado_civil:
+    case 'S':
+        estado_civil_str = 'Solteiro'
+    case 'C':
+        estado_civil_str = 'Casado'
+    case 'V':
+        estado_civil_str = 'Viúvo'
+    case _:
+        estado_civil_str = 'Estado civil inválido'
+
+print('Estado civil:', estado_civil_str)
