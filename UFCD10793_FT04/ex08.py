c67af73789e76a310e7d3e0d602fa820ae578575
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:22:11 +0000

    Exercício 8 - while loop - tabuada do 5

--- UFCD10793_FT04/ex08.py
+++ UFCD10793_FT04/ex08.py
@@ -1,11 +1,10 @@
 '''
-Exercício 8 - while loop
+Exercício 8 - while loop - tabuada do 5
 Elabora um programa para escrever no ecrã os números de 1 a 100 e os respetivos quadrados
 '''
 
-# Input
-
-numero = int(input('Tabuada do número? '))
+#numero = int(input('Tabuada do número? '))
+numero = 5
 quantidade = 10
 i = 0
 while i < quantidade:
