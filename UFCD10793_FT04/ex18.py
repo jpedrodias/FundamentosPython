f922c340ef56339dbcd58547842ffa7d20a88835
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:50:33 +0000

    Exercício 18 - loops modulo da divisão inteira

---
+++ UFCD10793_FT04/ex18.py
@@ -0,0 +1,11 @@
+'''
+Exercício 18 - loops modulo da divisão inteira
+Elabore um programa que calcule e mostre no ecrã os números pares entre 1 e 200.
+'''
+
+# Input
+MIN = 1
+MAX = 200
+for i in range(MIN, MAX+1):
+    if i % 2 == 0:
+        print(i, end=' ')
