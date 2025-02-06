a5cd7d070fb7092edb91bb4a8d23e143e8155bbb
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:37:33 +0000

    Exercício 15 - for loop

---
+++ UFCD10793_FT04/ex15.py
@@ -0,0 +1,11 @@
+'''
+Exercício 15 - for loop
+Escreve um programa que mostre no ecrã os 20 primeiros múltiplos de 5.
+no ecrã.
+'''
+
+# Input
+MIN = 1
+MAX = 20
+for i in range(MIN, MAX+1):
+    print(i * 5, end=' ')   
