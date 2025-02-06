708a84ed6083aedc5b68fe542277d386dcb001ea
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:27:54 +0000

    Exercício 11 - while loop

---
+++ UFCD10793_FT04/ex11.py
@@ -0,0 +1,15 @@
+'''
+Exercício 11 - while loop
+Escreve um programa que calcule a soma e o produto dos N primeiros números naturais.
+'''
+
+# Input
+quantidade = int(input('Quantos números deseja calcular? '))
+soma = 0
+produto = 1
+i = 1
+while i < quantidade:
+    soma += i
+    produto *= i
+    i += 1
+print(f'A soma dos números de 1 a {quantidade} é {soma} e o produto é {produto}.')
