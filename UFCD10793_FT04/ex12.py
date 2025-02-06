40d231e8725edc93b29cf7bb2ea94d1e20a2fab9
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:30:38 +0000

    Exercício 12 - while loop - fatorial

---
+++ UFCD10793_FT04/ex12.py
@@ -0,0 +1,14 @@
+'''
+Exercício 12 - while loop - fatorial
+Escreve um programa que calcule a soma e o produto dos N primeiros números naturais.
+'''
+
+# Input
+numero = int(input('Calculo do fatorial. Qual o o número? '))
+
+fatorial = 1
+i = 1
+while i <= numero:
+    fatorial *= i
+    i += 1
+print(f'O fatorial de {numero} é {fatorial}.')
