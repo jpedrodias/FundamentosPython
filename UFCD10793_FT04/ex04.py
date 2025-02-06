0834a70a89225fb18c3ec856809b0a7dffe345c9
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:10:50 +0000

    Exercício 4 - while loop

---
+++ UFCD10793_FT04/ex04.py
@@ -0,0 +1,13 @@
+'''
+Exercício 4 - while loop
+Escreve um programa para escrever no ecrã a palavra olá 100 vezes.
+'''
+
+# Input
+quantidade = 100
+pakavra = 'olá'
+i = 0
+while i < quantidade:
+    print(pakavra, end=' ')
+    i += 1
+print()
