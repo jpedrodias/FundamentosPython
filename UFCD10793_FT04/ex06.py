0ae675f186063c6b628540cf9c4cadcd4d895d11
Pedro Dias <jpedrodias@gmail.com>
2025-02-06 23:13:17 +0000

    Exercício 6 - while loop

---
+++ UFCD10793_FT04/ex06.py
@@ -0,0 +1,13 @@
+'''
+Exercício 6 - while loop
+Elabora um programa para escrever no ecrã os números de 1 a 100 e os respetivos quadrados
+'''
+
+# Input
+quantidade = 100
+i = 0
+while i < quantidade:
+    quadrado = (i+1) ** 2
+    print(f'O quadrado de {i+1} é { quadrado }.')
+    i += 1
+print()
