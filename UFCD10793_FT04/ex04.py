ea91a16be98cfd05bb903b97092de8cd062b241e
Pedro Dias <jpedrodias@gmail.com>
2025-02-08 12:55:52 +0000

    Exercício 4 - while loop - 100 vezes

--- UFCD10793_FT04/ex04.py
+++ UFCD10793_FT04/ex04.py
@@ -2,12 +2,16 @@
 Exercício 4 - while loop - 100 vezes
 Escreve um programa para escrever no ecrã a palavra olá 100 vezes.
 '''
+# em python como fazer clear do terminal
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
-# Input
+
 quantidade = 100
 palavra = 'olá'
 i = 0
 while i < quantidade:
     print(palavra, end=' ')
     i += 1
+    if i % 20 == 0: print()
 print()
