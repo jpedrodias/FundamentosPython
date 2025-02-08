878e6e8f4d3e97fc97590f4e5f2e30cff2424fce
Pedro Dias <jpedrodias@gmail.com>
2025-02-08 09:55:08 +0000

    Exercício 8 - Classificação de Triângulos

--- UFCD10793_FT03/ex08.py
+++ UFCD10793_FT03/ex08.py
@@ -3,26 +3,26 @@ Exercício 8 - Classificação de Triângulos
 Escreve um programa para classificar um triângulo de acordo com o comprimento dos 
 seus lados. Considere as seguintes informações: 
 • Triângulo equilátero: todos os lados possuem o mesmo comprimento; 
-• Triângulo escaleno: todos os lados possuem comprimento diferente; 
+• Triângulo escaleno: todos os lados possuem comprimento diferente;
 • Triângulo isósceles: caracterizado por ter dois lados com o mesmo comprimento.
 
 '''
 
 
 # Input
-lado1 = input("Insira o comprimento do lado 1: ")
-lado2 = input("Insira o comprimento do lado 2: ")
-lado3 = input("Insira o comprimento do lado 3: ")
+lado_a = input("Insira o comprimento do lado 1: ")
+lado_b = input("Insira o comprimento do lado 2: ")
+lado_c = input("Insira o comprimento do lado 3: ")
 
 # Converte os números para reais
-lado1 = float(lado1)
-lado2 = float(lado2)
-lado3 = float(lado3)
+lado_a = float(lado_a)
+lado_b = float(lado_b)
+lado_c = float(lado_c)
 
 # Process
-if lado1 == lado2 == lado3:
+if lado_a == lado_b == lado_c:
     print(f"O triângulo é equilátero.")
-elif lado1 != lado2 != lado3:
+elif lado_a != lado_b != lado_c:
     print(f"O triângulo é escaleno.")
 else:
     print(f"O triângulo é isósceles.")
