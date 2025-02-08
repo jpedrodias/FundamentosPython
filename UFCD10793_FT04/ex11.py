510d23e2d2293527c891b1d003590c632866a3ec
Pedro Dias <jpedrodias@gmail.com>
2025-02-08 13:34:57 +0000

    Exercício 11 - while loop - somatório e produtório

--- UFCD10793_FT04/ex11.py
+++ UFCD10793_FT04/ex11.py
@@ -3,7 +3,6 @@ Exercício 11 - while loop - somatório e produtório
 Escreve um programa que calcule a soma e o produto dos N primeiros números naturais.
 '''
 
-# Input
 quantidade = int(input('Quantos números deseja calcular? '))
 soma = 0
 produto = 1
