015131de25d8e7e20ced7064bb78420c0bb69129
Pedro Dias <jpedrodias@gmail.com>
2025-02-08 09:49:08 +0000

    Exercício 7 - while loop - soma dos N primeiros números naturais

--- UFCD10793_FT04/ex07.py
+++ UFCD10793_FT04/ex07.py
@@ -1,5 +1,5 @@
 '''
-Exercício 7 - while loop
+Exercício 7 - while loop - soma dos N primeiros números naturais
 Elabora um programa para calcula a soma dos N primeiros números naturais (1+2+3+...+N)
 em que N é um número introduzido pelo utilizador (NOTA: este programa poderia ser feito
 utilizando a fórmula da progressão aritmética, S = (1+N) * N/2, mas faz de conta que não
