34114e2d213a5bdf77e7bccc941ac7bab417e706
Pedro Dias <jpedrodias@gmail.com>
2025-02-08 09:55:54 +0000

    Exercício 8 - Classificação de Triângulos

--- UFCD10793_FT03/ex08.py
+++ UFCD10793_FT03/ex08.py
@@ -5,7 +5,6 @@ seus lados. Considere as seguintes informações:
 • Triângulo equilátero: todos os lados possuem o mesmo comprimento; 
 • Triângulo escaleno: todos os lados possuem comprimento diferente;
 • Triângulo isósceles: caracterizado por ter dois lados com o mesmo comprimento.
-
 '''
 
 
