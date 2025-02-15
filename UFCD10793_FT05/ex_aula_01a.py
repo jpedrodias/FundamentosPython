cf18fb5e9494797563981f4706bfee6b7f10397c
Pedro Dias <jpedrodias@gmail.com>
2025-02-15 09:28:19 +0000

    Exercício 1 - ciclo for e while

---
+++ UFCD10793_FT05/ex_aula_01a.py
@@ -0,0 +1,15 @@
+'''
+Exercício 1 - ciclo for e while
+Escreva um programa que imprima os números de 1 até 50.
+'''
+
+print('Solução 1: ')
+for i in range(1, 51):
+    print(i, end=' ')
+print()
+
+print('Solução 2:')
+i = 0
+while i < 51:
+    print(i, end=' ')
+    i += 1
