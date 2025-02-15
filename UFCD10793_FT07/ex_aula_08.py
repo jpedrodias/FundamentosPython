7b66b5f348d806deb6f4728a8cf05cf1bd8032d3
Pedro Dias <jpedrodias@gmail.com>
2025-02-15 11:49:14 +0000

    Exercício 8 - Verificar se um item existe

---
+++ UFCD10793_FT07/ex_aula_08.py
@@ -0,0 +1,10 @@
+'''
+Exercício 8 - Verificar se um item existe
+'''
+
+tuple1 = (10, 20, 30, 40, 50, 60, 70, 80)
+# checking whether item 50 exists in tuple
+print(50 in tuple1)
+# Output True
+print(500 in tuple1)
+# Output False
