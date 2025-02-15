7634564a1e039a537646dca692f69730348e09d2
Pedro Dias <jpedrodias@gmail.com>
2025-02-15 09:36:27 +0000

    Exercício 1d - listas

---
+++ UFCD10793_FT05/ex_aula_01d.py
@@ -0,0 +1,10 @@
+'''
+Exercício 1d - listas
+Escreva um programa que multiplique todos os elementos de uma lista.
+'''
+
+lista = [10, 10, 10]
+mul = 1
+for num in lista:
+    mul = mul*num
+    print(mul)
