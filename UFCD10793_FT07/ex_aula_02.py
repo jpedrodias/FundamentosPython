fbe1cb9348017b088986f6baef6332e8889c65ce
Pedro Dias <jpedrodias@gmail.com>
2025-02-15 10:45:55 +0000

    Exercício 2 - Tuplas

---
+++ UFCD10793_FT07/ex_aula_02.py
@@ -0,0 +1,18 @@
+'''
+Exercício 2 - Tuplas
+Criar uma tupla com um único item.
+'''
+
+# without comma
+single_tuple = ('Hello' )
+print(type(single_tuple))
+# Output class 'str'
+print(single_tuple)
+# Output Hello
+
+# with comma
+single_tuple1 = ('Hello' ,)
+# output class 'tuple'
+print(type(single_tuple1))
+# Output ('Hello' ,)
+print(single_tuple1)
