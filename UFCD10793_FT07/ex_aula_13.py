9ddcf40ca16e388fec9d09a7fdcf8997e570f7e9
Pedro Dias <jpedrodias@gmail.com>
2025-02-15 12:40:25 +0000

    Exercício 13 - Concatenar(juntar) duas tuplas – usar o operador +

---
+++ UFCD10793_FT07/ex_aula_13.py
@@ -0,0 +1,11 @@
+'''
+Exercício 13 - Concatenar(juntar) duas tuplas – usar o operador +
+'''
+
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = (3, 4, 5, 6, 7)
+
+# concatenate tuples using + operator
+tuple3 = tuple1 + tuple2
+print(tuple3)
+# Output (1, 2, 3, 4, 5, 3, 4, 5, 6, 7)
