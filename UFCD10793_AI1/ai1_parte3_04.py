11d9f9c7b98c12046ebb4c96642f6a647a143f0a
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 13:39:08 +0000

    Avaliação Intermédia 1 - Parte 3 - Exercício 3 - atualizar dados

---
+++ UFCD10793_AI1/ai1_parte3_04.py
@@ -0,0 +1,32 @@
+'''
+Avaliação Intermédia 1 - Parte 3 - Exercício 3 - atualizar dados
+'''
+
+print(__doc__)
+
+import sqlite3, time
+
+conn = sqlite3.connect('data/empresa.sqlite')
+cursor = conn.cursor()
+
+# 1. Atualizar o salário de Pedro Santos
+SQL_UPDATE_SALARIO = "UPDATE funcionarios SET salario = 3000.00 WHERE nome = 'Pedro Santos'"
+cursor.execute(SQL_UPDATE_SALARIO)
+
+# 2. Aumentar o salário de todos os funcionários em 5%
+#SQL_UPDATE_SALARIO = "UPDATE funcionarios SET salario = salario * 1.05"
+#cursor.execute(SQL_UPDATE_SALARIO)
+
+cursor.execute("SELECT * FROM funcionarios")
+funcionarios = cursor. fetchall()
+for funcionario in funcionarios:
+    id, nome, cargo, salario = funcionario
+    novo_salario = salario * 1.05
+    print(f'ID: {id}, Nome: {nome}, Cargo: {cargo}, Salário: {salario} -> {novo_salario}')
+    SQL_UPDATE_SALARIO = f"UPDATE funcionarios SET salario = {novo_salario} WHERE id = {id}"
+    cursor.execute(SQL_UPDATE_SALARIO)
+    conn.commit()    
+    time.sleep(1)
+
+conn.commit()
+conn.close()
