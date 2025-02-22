0627c554bd1aaae575a28f9988c9f74aa40ec143
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 10:20:13 +0000

    Avaliação Intermédia 1 - Exercício 6

--- UFCD10793_AI1/ai1_ex_06.py
+++ UFCD10793_AI1/ai1_ex_06.py
@@ -7,3 +7,13 @@ Trate erros como divisão por zero e valores inválidos
 
 from ai1_mytools import better_input
 
+number_a = better_input("Digite o primeiro número inteiro: ", 'Erro: Apenas números inteiros são permitidos.')
+number_b = better_input("Digite o segundo número inteiro: ", 'Erro: Apenas números inteiros são permitidos.', not_allowed={}) # permite a inserção de zero para testar a divisão por zero
+
+try:
+    result = number_a / number_b
+    print('A divisão de', number_a, 'por', number_b, 'é', result)
+except ZeroDivisionError:
+    print('Não é possível a divisão de por zero')
+finally:
+    pass
