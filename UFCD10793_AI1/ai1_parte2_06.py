6cab610db9d1683e546fa2d663c95db2ab93b710
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 13:05:11 +0000

    Avaliação Intermédia 1 - Parte 2 - Exercício 6

---
+++ UFCD10793_AI1/ai1_parte2_06.py
@@ -0,0 +1,16 @@
+'''
+Avaliação Intermédia 1 - Parte 2 - Exercício 6
+
+Cria ou faz download de um ficheiro CSV. De seguida cria um programa que leia o ficheiro CSV e mostre cada linha do mesmo.
+
+Ficheiro CSV retirado de https://www.kaggle.com/datasets/uciml/iris
+Instalada Extensão vscode: Rainbow CSV
+'''
+
+import csv
+
+filename = 'data/kaggle_datasets_ucim_iris.csv'
+with open(filename, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
