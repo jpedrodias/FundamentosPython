a6baf5f2eb1df901baece30570798e2caa94fd8b
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 09:54:48 +0000

    Avaliação Intermédia 1 - Exercício 3

--- UFCD10793_AI1/ai1_ex_03.py
+++ UFCD10793_AI1/ai1_ex_03.py
@@ -1,5 +1,33 @@
 '''
 Avaliação Intermédia 1 - Exercício 3
 
-Peça dois números ao utilizador e trate a divisão por zero.
+Verifique se um ficheiro existe antes de o abrir.
 '''
+
+# Exemplo 1
+import os
+
+try:
+    caminho = input("Introduza o caminho do ficheiro: ")
+    if os.path.exists(caminho):
+        with open(caminho, 'r') as f:
+            print(f.read())
+    else:
+        print("Erro: O ficheiro não foi encontrado.")
+except FileNotFoundError:
+    print("Erro: O ficheiro não foi encontrado.")
+except Exception as e:
+    print("Erro: inesperado", e)
+
+
+# Exemplo 2 - using try only here is needed
+import os
+caminho = input("Introduza o caminho do ficheiro: ")
+if os.path.exists(caminho):
+    try:
+        with open(caminho, 'r') as f:
+            print(f.read())
+    except Exception as e:
+        print("Erro: inesperado", e)
+else:
+    print("Erro: O ficheiro não foi encontrado.")
