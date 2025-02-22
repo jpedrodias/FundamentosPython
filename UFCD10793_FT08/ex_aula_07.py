8dd32230af2ce627574df947d5f65058f8af0b66
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 10:44:57 +0000

    Exercício Exemplo 7 - diferença simétrica de dois conjuntos

---
+++ UFCD10793_FT08/ex_aula_07.py
@@ -0,0 +1,30 @@
+'''
+Exercício Exemplo 7 - diferença simétrica de dois conjuntos
+'''
+
+#Diferença simétrica de dois conjuntos - A sem os elementos comuns a B
+# first set
+A = {2, 3, 5}
+
+# second set
+B = {1, 2, 6}
+
+# perform difference operation using &
+print('using ^:', A ^ B)
+
+# using symmetric_difference()
+print('using symmetric_difference():', A.symmetric_difference(B))
+
+#verificar se dois conjuntos são iguais
+
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {3, 5, 1}
+
+# perform difference operation using &
+if A == B:
+    print('Set A and Set B are equal')
+else:
+    print('Set A and Set B are not equal')
