a9a6df46b907d71366501788b2b84335a00db9e0
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 12:38:57 +0000

    Avaliação Intermédia 1 - Parte 2 - Exercício 3

--- UFCD10793_AI1/ai1_parte2_03.py
+++ UFCD10793_AI1/ai1_parte2_03.py
@@ -3,11 +3,12 @@ Avaliação Intermédia 1 - Parte 2 - Exercício 3
 
 Criar um programa que escreva três linhas num ficheiro novo.
 '''
+NEW_LINE = "\n"
 
 filename = "z_novo_ficheiro.txt"
 with open(filename, "w") as ficheiro:
-    ficheiro.write("Linha 1")
-    ficheiro.write("Linha 2")
+    ficheiro.write("Linha 1" + NEW_LINE)
+    ficheiro.write("Linha 2" + NEW_LINE)
     ficheiro.write("Linha 3")
 
 print("Ficheiro criado com sucesso.", filename)
