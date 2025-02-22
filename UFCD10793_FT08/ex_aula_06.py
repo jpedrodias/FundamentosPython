e586ac8f338425c44635f1b2f4c901ca3057574e
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 10:43:24 +0000

    Exercício Exemplo 5 - interseção de conjuntos

---
+++ UFCD10793_FT08/ex_aula_06.py
@@ -0,0 +1,29 @@
+'''
+Exercício Exemplo 5 - interseção de conjuntos
+'''
+
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {1, 2, 3}
+
+# perform intersection operation using &
+print('Intersection using &:', A & B)
+
+# perform intersection operation using intersection()
+print('Intersection using intersection():', A.intersection(B))
+
+#diferença de dois conjuntos - A sem B
+
+# first set
+A = {2, 3, 5}
+
+# second set
+B = {1, 2, 6}
+
+# perform difference operation using &
+print('Difference using &:', A - B)
+
+# perform difference operation using difference()
+print('Difference using difference():', A.difference(B))
