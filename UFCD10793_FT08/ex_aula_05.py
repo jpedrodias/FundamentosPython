4626af4e9bb69d84f26d08cec0fd42992e3b26cd
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 10:40:04 +0000

    Exercício Exemplo 5 - método update()

---
+++ UFCD10793_FT08/ex_aula_05.py
@@ -0,0 +1,40 @@
+'''
+Exercício Exemplo 5 - método update()
+'''
+print(__doc__)
+
+#método update()
+companies = {'Lacoste', 'Ralph Lauren'}
+tech_companies = ['apple', 'google', 'apple']
+
+companies.update(tech_companies)
+
+print(companies)
+
+# Output: {'google', 'apple', 'Lacoste', 'Ralph Lauren'}
+
+#Remover um elemento de um conjunto
+#Usamos o discard()método para remover o elemento especificado de um conjunto. Por exemplo,
+
+languages = {'Swift', 'Java', 'Python'}
+
+print('Initial Set:',languages)
+
+# remove 'Java' from a set
+removedValue = languages.discard('Java')
+
+print('Set after remove():', languages)
+
+#Iterar sobre um conjunto em Python
+fruits = {"Apple", "Peach", "Mango"}
+
+# for loop to access each fruits
+for fruit in fruits:
+    print(fruit)
+
+#Número de elementos de um conjunto
+even_numbers = {2,4,6,8}
+print('Set:',even_numbers)
+
+# find number of elements
+print('Total Elements:', len(even_numbers))
