37566d400419db19167acfb9b523cbe00f713d14
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 12:57:59 +0000

    Avaliação Intermédia 1 - Parte 2 - Exercício 2

--- UFCD10793_AI1/ai1_parte2_02.py
+++ UFCD10793_AI1/ai1_parte2_02.py
@@ -4,7 +4,7 @@ Avaliação Intermédia 1 - Parte 2 - Exercício 2
 Modificar o exercício anterior para exibir o conteúdo linha por linha.
 '''
 
-filename = "z_exemplo.txt"
+filename = "data/exemplo.txt"
 with open(filename, "r") as ficheiro:
     for linha in ficheiro:
         print(linha.strip())
