d8d98dd0a86782858e813d9939f30a699131a5eb
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 10:28:54 +0000

    Avaliação Intermédia 1 - Exercício 7

---
+++ UFCD10793_AI1/ai1_ex_07.py
@@ -0,0 +1,14 @@
+'''
+Avaliação Intermédia 1 - Exercício 7
+
+* open() & with open() - abrir o ficheiro e abrir o ficheiro em contexto
+    * read()
+    * readline()
+    * readlines()
+    * write()
+    * writelines()
+    * close()
+* mmap() - mm = mmap.mmap(f.fileno(), 0) - permite mapear ficheiros firetamente na memória
+
+
+'''
