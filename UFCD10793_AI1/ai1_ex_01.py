589192b94f0c6b69a7b7726abd2dd4f0b2a61e04
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 09:36:07 +0000

    Avaliação Intermédia 1 - Exercício 1

--- UFCD10793_AI1/ai1_ex_01.py
+++ UFCD10793_AI1/ai1_ex_01.py
@@ -4,7 +4,16 @@ Avaliação Intermédia 1 - Exercício 1
 Escreva um programa que pede ao utilizador um número inteiro e trata erros de entrada.
 '''
 
-def my_imput(prompt, error_message="Erro! Introduza um número inteiro.", ):
+def better_input(prompt, error_message="Erro! Introduza um número inteiro.", data_type=int):
     done = False
     while not done:
-        user_imput = input(prompt)
+        user_input = input(prompt)
+        try:
+            data = data_type(user_input)
+            done = True
+        except ValueError:
+            print(error_message)
+    
+    return data
+
+number = better_input("Introduza um número inteiro: ")
