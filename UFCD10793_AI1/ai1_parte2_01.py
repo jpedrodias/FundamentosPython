6c9820950d3141c7aa8b35dc8bd992285a82b70f
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 12:34:07 +0000

    Avaliação Intermédia 1 - Parte 2 - Exercício 1

--- UFCD10793_AI1/ai1_parte2_01.py
+++ UFCD10793_AI1/ai1_parte2_01.py
@@ -1,7 +1,11 @@
 '''
 Avaliação Intermédia 1 - Parte 2 - Exercício 1
 
-
+Criar um programa que leia um ficheiro de texto e exibir o seu conteúdo.
 '''
 
 print(__doc__, )
+
+with open("exemplo.txt", "r") as ficheiro:
+    conteudo = ficheiro.read()
+    print(conteudo)
