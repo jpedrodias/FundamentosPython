e4db88d34042f6d61c49995cef5a3c9e35999ea8
Pedro Dias <jpedrodias@gmail.com>
2025-02-22 21:39:53 +0000

    Avaliação Intermédia 1 - Parte 3 - Exercício 6 - CRUD System com SQLite & Flask

--- UFCD10793_AI1/ai1_parte3_06_FlaskApp.py
+++ UFCD10793_AI1/ai1_parte3_06_FlaskApp.py
@@ -1,9 +1,12 @@
 '''
 Avaliação Intermédia 1 - Parte 3 - Exercício 6 - CRUD System com SQLite & Flask
 
+* pip install flask
 
 * cd flaskapp
+
 * python app.py
+
 * abrir http://localhost:5000/ no browser
 
 
