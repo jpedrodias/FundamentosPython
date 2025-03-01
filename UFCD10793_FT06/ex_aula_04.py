cc9505771a49c621f11f01f7ebee3275befe3830
Pedro Dias <jpedrodias@gmail.com>
2025-03-01 11:44:01 +0000

    Exercício 4 - listas

--- UFCD10793_FT06/ex_aula_04.py
+++ UFCD10793_FT06/ex_aula_04.py
@@ -1,7 +1,7 @@
 '''
 Exercício 4 - listas
 Considera a seguinte lista:
-    nums=[10, 2.5, 7, 11, 7.9, "Python", True,6, 5.8 , "Listas"]
+    nums = [10, 2.5, 7, 11, 7.9, 'Python', True, 6, 5.8, 'Listas']
 
 Efetua um programa em python que:
 a. Imprima a quantidade de inteiros, floats, strings e boleanos na lista;
@@ -9,7 +9,7 @@ b. Efetua a média de todos os valores inteiros na lista.
 c. Crie e retorne uma nova lista só com os valores inteiros
 '''
 
-nums = [10, 2.5, 7, 11, 7.9, 'Python', True,6, 5.8 , 'Listas']
+nums = [10, 2.5, 7, 11, 7.9, 'Python', True, 6, 5.8, 'Listas']
 
 # a. Imprima a quantidade de inteiros, floats, strings e boleanos na lista;
 inteiros = 0
