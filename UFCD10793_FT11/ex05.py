ee8af466ccacc0f7c89dea8874c71f1342f88618
Pedro Dias <jpedrodias@gmail.com>
2025-03-01 10:32:36 +0000

    Exercício 5 - funções

--- UFCD10793_FT11/ex05.py
+++ UFCD10793_FT11/ex05.py
@@ -4,17 +4,27 @@ Escreve uma função em Python que, dada uma lista de elementos, devolva
 essa mesma lista, mas sem elementos repetidos.
 '''
 
-from os import chdir, getcwd, path
-
-print('A pasta antes de chdir', getcwd())
-workdir = path.dirname(path.abspath(__file__))
-chdir(workdir)
-
-print('A pasta depois de chdir', getcwd())
-
-
-def remove_repetidos(lista):
+def remove_repetidos_por_index(lista):
+    '''Remove elementos repetidos de uma lista, mantendo o primeiro elemento
+    e removendo os restantes.
+    
+    WARNIG: Em python as listas são passadas por referência, por isso, a lista
+    original é alterada. Para manter a lista original, deve-se passar uma cópia da lista.
+    '''
     to_remove = []
-    for item in lista:
-        if lista.count(item) > 1:
-            to_remove.append(item)
+
+    new_list = lista.copy()
+
+    for index, item in enumerate(lista):
+        if lista[index:].count(item) > 1:
+            to_remove.append(index)
+    for index in reversed(to_remove):
+        new_list.pop(index)
+    return new_list
+
+lista = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+new_list = remove_repetidos_por_index(lista)
+    
+print(lista)
+print(new_list)
+
