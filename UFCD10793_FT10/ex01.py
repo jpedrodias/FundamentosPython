68ca4fd577440d7fac131fe433388444df7686d6
Pedro Dias <jpedrodias@gmail.com>
2025-03-01 09:09:25 +0000

    Exercício 1 - Strings

---
+++ UFCD10793_FT10/ex01.py
@@ -0,0 +1,29 @@
+'''
+Exercício 1 - Strings
+'''
+
+
+txt =" uFcd - proGRAMação eM pyTHON "
+
+#Imprimir texto
+
+print(txt)
+
+#Imprimir Texto sem espaçamento inicial
+
+txt=txt.strip()
+print(txt)
+#Imprimir frase até à palavra na 13ª posição
+
+print(txt[:13])
+
+# Imprimir frase a partir da palavra na 13ª posição
+print(txt[-5:])
+
+# Imprimir frase em maiúsculas
+txt = txt.upper()
+print(txt)
+
+# Formatação de strings
+nome = "Sandra Oliveira"
+print(f"O {nome} gosta muito da {txt}.")
