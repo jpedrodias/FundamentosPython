d54bb4c8c76b38c6a82dcd47eaecb7eb9be4981a
Pedro Dias <jpedrodias@gmail.com>
2025-03-15 12:46:36 +0000

    Exercício 2 - funções (com fancy colores)

--- UFCD10793_FT11/ex02.py
+++ UFCD10793_FT11/ex02.py
@@ -17,7 +17,7 @@ def quadrado(lado):
     return output
 
 # Testes
-fprint(quadrado(1), cor=Cores.RED) # Área: 1 Perímetro: 4
+fprint(quadrado(1), cor=Cores.RED)  # Área: 1 Perímetro: 4
 fprint(quadrado(2), cor=Cores.BLUE) # Área: 4 Perímetro: 8
-fprint(quadrado(3), cor=Cores.RED) # Área: 9 Perímetro: 12
+fprint(quadrado(3), cor=Cores.RED)  # Área: 9 Perímetro: 12
 fprint(quadrado(4), cor=Cores.BLUE) # Área: 16 Perímetro: 16
