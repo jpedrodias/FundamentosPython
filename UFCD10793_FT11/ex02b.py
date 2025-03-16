10ff3a17071110da1a71a0eea8cfe478533b97c0
Pedro Dias <jpedrodias@gmail.com>
2025-03-16 23:38:23 +0000

    My Fancy Terminal

--- UFCD10793_FT11/ex02b.py
+++ UFCD10793_FT11/ex02b.py
@@ -2,28 +2,7 @@
 Exercício 2 - funções com substituição da função interna print
 '''
 
-import builtins
-
-class Cores:
-    RESET = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-    YELLOW = '\033[93m'
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    BLUE = '\033[94m'
-
-def print(*args, **kwargs):
-    cor = kwargs.get('cor', Cores.RESET)
-    end = kwargs.get('end', '\n')
-    texto = ' '.join(map(str, args))
-    fancy = cor + texto + Cores.END 
-    builtins.__dict__['__original_print__'](fancy, end=end)  # evita recursão infinita
-
-# guardar referência original da função print
-builtins.__original_print__ = builtins.print
-builtins.print = print  # substituir print por fprint
+from my_fancy_terminal import print, Colors as Cores
 
 def tipo_triangulo(a, b, c):
     if a == b == c:
@@ -34,11 +13,11 @@ def tipo_triangulo(a, b, c):
         return "Escaleno"
 
 # agora o 'print' já utiliza cores diretamente
-print("Triangulo Equilátero", cor=Cores.BLUE)
+print("Triangulo Equilátero", color=Cores.BLUE)
 print(1, 1, 1, tipo_triangulo(1,1,1))
 
-print("Triangulo Isósceles", cor=Cores.RED)
+print("Triangulo Isósceles", color=Cores.RED)
 print(1, 1, 2, tipo_triangulo(1,1,2))
 
-print("Triangulo Escaleno", cor=Cores.GREEN)
-print(1, 2, 3, tipo_triangulo(1,2,3), cor=Cores.RED)
+print("Triangulo Escaleno", color=Cores.GREEN)
+print(1, 2, 3, tipo_triangulo(1,2,3), color=Cores.RED)
--- UFCD10793_FT11/my_fancy_terminal.py
+++ UFCD10793_FT11/my_fancy_terminal.py
@@ -1,10 +1,21 @@
 '''
-FANCY TERMINAL
-* class Cores
+My Fancy Terminal
+* class Colors
 * def fprint
-'''
+* def print - replaces original print to add the "color" parameter
 
-class Cores:
+
+Usage:
+from my_fancy_terminal import print, Colors as Cores
+print('Hello World!', color=Cores.RED)
+
+'''
+import builtins
+
+# backup a função original de print
+builtins.__original_print = print
+
+class Colors:
     RESET = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
@@ -15,15 +26,18 @@ class Cores:
     BLUE = '\033[94m'
 
 def fprint(*args, **kwargs):
-    
     # kargs
-    cor = kwargs.get('cor', Cores.RESET)
+    color = kwargs.get('color', Colors.RESET)
     end = kwargs.get('end', '\n')
 
     # args
     texto = ' '.join(map(str, args))
 
-    fancy = cor + texto + Cores.RESET 
-    print(fancy, end=end)
+    fancy = color + texto + Colors.RESET
+    builtins.__original_print(fancy, end=end)
 
-    
+def print(*args, **kwargs):
+    fprint(*args, **kwargs)
+
+if __name__ == '__main__':
+    print('Hello World!', color=Colors.RED)
