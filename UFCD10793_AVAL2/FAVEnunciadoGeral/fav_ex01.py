9804916b23dca7504f55e2285dd65d286efb15c1
Pedro Dias <jpedrodias@gmail.com>
2025-03-22 15:00:43 +0000

    Exercício 1 - FAV Enunciado Geral

---
+++ UFCD10793_AVAL2/FAVEnunciadoGeral/fav_ex01.py
@@ -0,0 +1,29 @@
+'''
+Exercício 1 - FAV Enunciado Geral
+'''
+
+# lê uma data no formato DD/MM/AAAA
+#data_str = input("Insere uma data no formato DD/MM/AAAA: ")
+data_str = '05/04/2025'
+
+
+# Sem utilizar módulos
+# separa a data em dia, mês e ano
+dia_str, mes_str, ano_str = data_str.split("/")
+dia, mes, ano = map(int, [dia_str, mes_str, ano_str])
+
+print(f'{ano:4d}-{mes:02d}-{dia:02d}')
+
+
+# ALTERNATIVA USANDO módulo datetime
+from datetime import datetime
+
+try:
+    data = datetime.strptime(data_str, "%d/%m/%Y")
+except ValueError:
+    data = None
+
+if data:
+    print(data.strftime("%Y-%m-%d"))
+else:
+    print("Data inválida")
