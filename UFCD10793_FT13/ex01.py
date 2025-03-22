bff3ea044b9d415a31302b7795c82d51d320cbbd
Pedro Dias <jpedrodias@gmail.com>
2025-03-22 09:05:03 +0000

    Exercício 1

--- UFCD10793_FT13/ex01.py
+++ UFCD10793_FT13/ex01.py
@@ -20,33 +20,37 @@ print(' * ler ficheiro cast.csv para dataframe casts')
 casts = pd.read_csv('data/cast.csv', index_col=None)
 
 
-# dados estatisticos do conjunto de dados
+# .describe - mostra estatísticas do conjunto de dados
 print(casts.describe())
 
-# dados estatisticos do conjunto
+# .describe (include='all') - mostra estatísticas do conjunto de dados incluindo todos os tipos de dados
 print(casts.describe(include='all'))
 
-# imprimir as cinco primeiras linhas do dataframe casts
+# .head() - mostra as primeiras 5 linhas do conjunto de dados
 print(casts.head())
 
-# ler ficheiro titles.csv para dataframe titles
+
+
+# FICHEIRO data/titles.csv
+# .read_csv - lê o ficheiro csv e cria um dataframe
 titles = pd.read_csv('data/titles.csv', index_col =None)
 
 
-# imprimir as cinco últimas linhas do dataframe titles
+#. tail() - mostra as últimas 5 linhas do conjunto de dados
 print(titles.tail())
 
-#alterar numero de linhas e colunas a visualizar sempre que se imprime todo o dataframe
+# .set_option - alterar numero de linhas e colunas a visualizar sempre que se imprime todo o dataframe
 pd.set_option('display.max_rows', 10, 'display.max_columns', 10)
 
 # imprimir dataframe titles
 print(titles)
 
-# atribuir à variável tamanho o numero total d linhas do dataframe titles
+
+# numero total d linhas do dataframe titles
 tamanho=len(titles)
 
 # imprimir tamanho
-print(tamanho)
+print('tamanho do dataframe:', tamanho)
 
 # imprimir primeiras três linhas do dataframe titles
 print(titles.head(3))
@@ -62,3 +66,75 @@ casts.info()
 
 # imprimir linha de indice 0 no dataframe titles
 print(titles.iloc[0])
+
+
+t = titles
+#obter dataframe movies90 filtrando o dataframe t selecionando os registos cujo ano é superior e igual a 1990 e inferior a 2000
+movies90 = t[ (t['year']>=1990) & (t['year']<2000)]
+print(movies90.head())
+
+#obter dataframe macbeth a partir do dataframe t cujo valor na coluna title é igual a Macbeth
+macbeth = t[ t['title'] == 'Macbeth']
+print(macbeth.head())
+
+#obter dataframe macbeth a partir do dataframe t cujo valor na coluna title é igual a Macbeth e ordenar por index
+macbeth = t[ t['title'] == 'Macbeth'].sort_index()
+print(macbeth.head())
+
+#obter dataframe macbeth a partir do dataframe t cujo valor na coluna title é igual a Macbeth e ordenar pelos values
+macbeth = t[ t['title'] == 'Macbeth'].sort_values('year')
+print(macbeth.head())
+
+#obter dataframe macbeth a partir do dataframe t cujo valor na coluna title é igual a Macbeth e ordenar pelos values
+macbeth = t[ t['title'] == 'Macbeth'].sort_values('year')
+print(macbeth.head())
+
+#devolve true em cada elemento da linha correspondente à coluna n se o valor for nulo e false caso contrário
+print(c['n'].isnull().head())
+
+#devolve false em cada elemento da linha correspondente à coluna n se o valor for nulo e true caso contrário
+print(c['n'].notnull().head())
+
+#devolve as primeiras linhas com valor nulo na coluna n
+print(c[c['n'].isnull()].head(3))
+
+#substitui os valores nulos de n por 'NA'
+c_fill = c[c['n'].isnull()].fillna('NA')
+print(c_fill.head(2))
+
+
+t = titles
+#imprime as linhas do dataframe cujo valor afetos à coluna title é igual a 'Maa'
+print(t[t['title'] == 'Maa'])
+
+#imprime as linhas do dataframe cujp valor afetos à coluna title é inicia com a 'Maa'
+print(t[t['title'].str.startswith("Maa ")].head(3))
+
+
+#conta os valores não nulos presentes na coluna year
+print(t['year'].value_counts().head())
+
+c = casts
+
+#obtem o dataframe cf a partir de c filtrando os dados que verifiquem a condição do autor ser 'Aaron Abrams'
+cf = c[c['name'] == 'Aaron Abrams'] 
+
+#Agrupar os dados do dataframe anterior pela coluna year
+print(cf.groupby(['year']).size().head())
+
+
+#group by com múltiplas colunas
+gbmultiplecolumns = cf.groupby(['year', 'title']).size()
+gbmultiplecolumns.head()
+
+#leitura dos dados do ficheiro release_dates.csv para o dataframe release
+release = pd.read_csv('release_dates.csv', index_col=None)
+print(release.head())
+
+c_amelia = casts[ casts['title'] == 'Amelia']
+print(c_amelia.head())
+print(release [ release['title'] == 'Amelia' ].head())
+
+#imprimeir os primeiras cinco linhas do merge (fusão) c_amelia com o dataframe
+release
+print(c_amelia.merge(release).head())
