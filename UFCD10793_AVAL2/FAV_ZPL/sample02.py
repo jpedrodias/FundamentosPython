444692ee5a90a802ade5fd5dda29c0a751247de9
Pedro Dias <jpedrodias@gmail.com>
2025-03-28 20:06:14 +0000

    Sample 2 - FAV Enunciado ZPL

--- UFCD10793_AVAL2/FAV_ZPL/sample02.py
+++ UFCD10793_AVAL2/FAV_ZPL/sample02.py
@@ -1,3 +1,27 @@
+'''
+Sample 2 - FAV Enunciado ZPL
+'''
 import os, csv, requests
 
-# 
+# change the working directory to the script's directory
+workdir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(workdir)
+
+
+# Define a pasta de saída
+os.makedir('data', exist_ok=True)
+
+# Configuração da impressora e etiqueta
+from dataclasses import dataclass
+
+@dataclass
+class Settings:
+    dpi = '8dpmm'
+    size = '4x6'
+    rotation = '0'
+    labelary_url = f"https://api.labelary.com/v1/printers/{dpi}/labels/{size}/{rotation}/"
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+
+# Lê os dados do CSV e gera etiquetas
+
