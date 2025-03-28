8632b16dbdd48c697bcad626dfcff4d6d878e382
Pedro Dias <jpedrodias@gmail.com>
2025-03-28 19:59:34 +0000

    chdir to worddir added

--- UFCD10793_AVAL2/FAV_ZPL/sample01.py
+++ UFCD10793_AVAL2/FAV_ZPL/sample01.py
@@ -1,4 +1,10 @@
-import requests
+import os, requests
+
+
+# change the working directory to the script's directory
+workdir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(workdir)
+
 
 zpl = "^XA^F050,50^A0N,50,50^FDOla Mundo!^FS^XZ"
 url = "https://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/"
